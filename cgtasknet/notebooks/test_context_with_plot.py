01241285477f226d1881cb7d4267f092a60e4d4c
Pugavkomm <slavapugavko2@gmail.com>
2021-12-27 01:39:57 +0300

    Update

--- cgtasknet/notebooks/test_context_with_plot.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-
-from cgtasknet.tasks.tasks import ContextDM
-
-task = ContextDM()
-inputs, outputs = task.dataset(1)
-print(f"inputs: {inputs.shape} outputs: {outputs.shape}")
-plt.plot(inputs[:, 0, 0])
-plt.show()
